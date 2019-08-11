7b900692bfa662c153034faf5ec5f7ae3f8e42c5
Oskar Rainer <rainer.oskar@gmail.com>
2019-08-11 17:45:45 +0200

    add base theme

---
+++ admin.php
@@ -0,0 +1,23 @@
+<?php
+
+$app->on('admin.init', function () {
+  // add custom theme
+  $this->helper('admin')->addAssets('theme:assets/styles.css');
+
+  // disable time widget in dashboard
+  $this->on('admin.dashboard.widgets', function($widgets) {
+    foreach($widgets as $key => $widget) {
+      if ($widget['name'] == 'time') {
+        unset($widgets[$key]);
+        break;
+      }
+    }
+  }, 0);
+
+  // Set default group in entry view to "Main" (default: "All")
+  // When the page loads, `this.group` is an empty string. After the first
+  // call of `toggleGroup()` it is 'GroupName' or false.
+  $this->on('collections.entry.aside', function() {
+    echo '<span if="{ group === \'\' && !(group = \'Main\') }" class="">test</span>';
+  });
+});
